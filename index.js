750cfc2000ca8232c6c2f270e795dec1e210a84d
ing <inyoman_gurnitha@yahoo.com>
2020-09-06 16:20:57 +0700

    5. Creating Page Routes

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-//Import modules(express)
+//Import modules(express, path)
 const express = require('express')
+const path = require('path')
 
 // Express app
 const app = new express()
 
 // Static assets folder
 app.use(express.static('public'))
+
+// http://localhost:4000/
+app.get('/',(req,res)=>{
+	res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+// http://localhost:4000/about
+app.get('/about',(req,res)=>{
+	res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+// http://localhost:4000/post
+app.get('/post',(req,res)=>{
+	res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+// http://localhost:4000/contact
+app.get('/contact',(req,res)=>{
+	res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
 
 // Port:
 app.listen(4000, ()=>{
