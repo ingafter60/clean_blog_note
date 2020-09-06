7cd768fc2482d4a7e0ee88ddaa0d06497070fee8
ing <inyoman_gurnitha@yahoo.com>
2020-09-06 16:16:03 +0700

    4. public folder for serving static files

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require('express')
 // Express app
 const app = new express()
 
-// Port:400
+// Static assets folder
+app.use(express.static('public'))
+
+// Port:
 app.listen(4000, ()=>{
 console.log('App listening on port 4000')
 })
