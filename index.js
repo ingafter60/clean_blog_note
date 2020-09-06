91a686cce9ff8fdc6db5cce36020404f5a9c6b0e
ing <inyoman_gurnitha@yahoo.com>
2020-09-06 16:32:29 +0700

    7. Using EJS as templating engines and rename folder and files extensions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//Import modules(express, path)
+//Import modules(express, path, ejs )
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 // Express app
 const app = new express()
@@ -8,24 +9,31 @@ const app = new express()
 // Static assets folder
 app.use(express.static('public'))
 
+// EJS as templating engines
+app.set('view engine','ejs')
+
 // http://localhost:4000/
 app.get('/',(req,res)=>{
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+	// res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+	res.render('index');
 })
 
 // http://localhost:4000/about
 app.get('/about',(req,res)=>{
-	res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+	// res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+	res.render('about');
 })
 
 // http://localhost:4000/post
 app.get('/post',(req,res)=>{
-	res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+	// res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+	res.render('post');
 })
 
 // http://localhost:4000/contact
 app.get('/contact',(req,res)=>{
-	res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+	// res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+	res.render('contact');
 })
 
 // Port:
